c212dcd25b271ada46bc50a6d0fc50f558e3a28c
Awale11 <abdisalam902@gmail.com>
2026-07-06 20:22:26 +0200

    Added exercise 3

---
+++ exercise 3/main3.js
@@ -0,0 +1,12 @@
+let isSunny = true;
+let isWeekend = false;
+
+// AND &&
+console.log(isSunny && isWeekend);
+
+// OR ||
+console.log(isSunny || isWeekend);
+
+// NOT !invert
+console.log(!isWeekend);
+
