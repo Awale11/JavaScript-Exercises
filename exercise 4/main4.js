f96c3edbefd1c540ebb9574d0307ead726f064a7
Awale11 <abdisalam902@gmail.com>
2026-07-07 21:09:23 +0200

    Added exercise 4

---
+++ exercise 4/main4.js
@@ -0,0 +1,34 @@
+// Function Basicis
+
+  function addTotal(income,expenditure) {
+    console.log(income + expenditure)
+  }
+
+  addTotal(7,3);
+  addTotal(9,20);
+  addTotal(100,89);
+  addTotal(49,67);
+
+  // Function Expressions
+
+  const addTotal = function(income,expenditure) {
+    console.log(income + expenditure)
+  };
+  addTotal(7,3);
+   addTotal(9,20);
+  addTotal(100,89);
+  addTotal(49,67);
+  
+
+
+
+
+
+
+
+
+//   const greets = function(name) {
+//     console.log("Asc " + name)
+// };
+
+// greets("abdi");
