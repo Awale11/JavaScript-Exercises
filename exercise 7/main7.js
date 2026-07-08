dd50ef4dd3dbcb0dd7b0cf806a1e2ad1e2d6ba6b
Awale11 <abdisalam902@gmail.com>
2026-07-08 20:45:31 +0200

    Added exercise 7

---
+++ exercise 7/main7.js
@@ -0,0 +1,24 @@
+// Array
+
+let colors = ["Gray", "Green", "Brown"];
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+
+// Array Methodos
+
+console.log("before push");
+console.log(colors);
+
+console.log("after push");
+colors.push("Red");
+console.log(colors);
+
+console.log("before shift");
+console.log(colors);
+
+console.log("afrer shift");
+colors.shift();
+console.log(colors);
+
