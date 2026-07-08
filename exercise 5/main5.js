a56dc6b4b090c2f43defeae66066fb3ed3c90b96
Awale11 <abdisalam902@gmail.com>
2026-07-08 16:31:18 +0200

    Added exercise 5

---
+++ exercise 5/main5.js
@@ -0,0 +1,16 @@
+// Arrow Functions
+
+const addTotal = (income,expenditure) => {
+     console.log(income + expenditure)
+};
+
+addTotal(7,3);
+addTotal(9,20);
+addTotal(100,89);
+addTotal(49,67);
+
+
+
+
+
+
