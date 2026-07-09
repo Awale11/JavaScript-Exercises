21050efa0feded4cf83be175431b205ee426d988
Awale11 <abdisalam902@gmail.com>
2026-07-09 21:06:03 +0200

    Added exercise 10

---
+++ exercise 10/main.10.js
@@ -0,0 +1,21 @@
+// Arrays of Objects 
+
+let library = [
+    {
+        title: "The Man and the Poet",
+        author: "Hadraawi",
+        year: 2013,
+    },
+    {
+        title: "And Then There Were None",
+        author: "Agtha Christie",
+        year: 1939,
+    },
+]
+
+// The title of the first book
+console.log(library[0].title);
+
+// The author of the second book
+console.log(library[1].author);
+
