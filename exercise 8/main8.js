d5145f782d93838a6ddac89b7ef625a1d2f065af
Awale11 <abdisalam902@gmail.com>
2026-07-09 18:52:39 +0200

    Added exercise 8

---
+++ exercise 8/main8.js
@@ -0,0 +1,24 @@
+// Ojects
+
+let car = {
+    make:  2020,
+    model: "BMW",
+    year: 2024,
+}
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+
+
+// My own exercise
+
+let country = {
+    name: "Somali",
+    capitalCity: "moqdisho",
+    religion: "islam"
+}
+
+console.log(country["name"]);
+console.log(country["capitalCity"]);
+console.log(country["religion"]);
