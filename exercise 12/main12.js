eefd529f8202a14766592da76ac65b0f82ab293b
Awale11 <abdisalam902@gmail.com>
2026-07-10 21:00:15 +0200

    Added exercise 12

---
+++ exercise 12/main12.js
@@ -0,0 +1,21 @@
+// Meaningful Example Using Arrays
+
+const fruits = ["apple", "banana", "cherry"]
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+// Using "LOOP FOR"
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i])
+}
+
+// My own exercise
+
+let webLanguages = ["html", "css", "javascript"]
+
+for (let i = 0; i < webLanguages.length; i++) {
+    console.log(webLanguages[i])
+}
