7623ec9870b788bd3bf148ac27a088f64e24bb88
Awale11 <abdisalam902@gmail.com>
2026-07-11 17:00:25 +0200

    Added exercise 16

---
+++ exercise 16/main16.js
@@ -0,0 +1,12 @@
+// Conditional Statements(if-else)
+
+
+//Write an if-else statement that logs "You passed!" if the variable score is 50 or more, and "You failed" otherwise.
+
+let score = 50;
+
+if(score >=50){
+    console.log("You paased!");
+} else{
+    console.log("You failed")
+}
