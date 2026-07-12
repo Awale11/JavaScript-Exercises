09428852d8e2302d238fc03dd614f71c07a0f8fd
Awale11 <abdisalam902@gmail.com>
2026-07-12 06:31:23 +0200

    Added exercise 18

---
+++ exercise 18/main18.js
@@ -0,0 +1,7 @@
+// Ternary operator
+
+// Use the ternary operator to log "Pass" if the variable grade is 60 or more, and "Fail" otherwise.
+
+let grade = 60;
+let message =  grade >= 60 ? "Pass"  : "Fail"
+console.log(message)
