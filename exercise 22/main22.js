eecdc509b9126f462e95c08b077d98a0f8754491
Awale11 <abdisalam902@gmail.com>
2026-07-13 20:58:24 +0200

    Added exercise 22

---
+++ exercise 22/main22.js
@@ -0,0 +1,12 @@
+// ES6 Basics
+// Destructuring
+
+// Use array destructuring to extract the first and third elements from the array ["red", "green", "blue"].
+
+
+const colors = ["red", "green", "blue"];
+
+const [first, green, third] = colors
+
+console.log(first);
+console.log(third);
