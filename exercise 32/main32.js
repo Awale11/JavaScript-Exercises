2d8bc77fea41b157d74a58658f566f0bb376d2ba
Awale11 <abdisalam902@gmail.com>
2026-07-18 16:45:53 +0200

    Added exercise 32

---
+++ exercise 32/main32.js
@@ -0,0 +1,15 @@
+// DOM
+
+// Use querySelector and querySelectorAll to select elements from an HTML structure of your choice. Log the selected elements to the console to verify the selections.
+
+const header = document.querySelector('#head');
+console.log(header);
+
+const paragraph = document.querySelectorAll('.paragraph');
+console.log(paragraph);
+
+const list = document.querySelectorAll('.list');
+console.log(list);
+
+const button = document.querySelector('button');
+console.log(button);
